6075bd42048b56d3d0d24696d6fc9f1ddef55947
nikpe <nikpe@NIKO_PC>
2021-10-16 23:41:51 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Random messi = new Random();
+            Console.Write("-");
+            for(int i = 0; i<1984; i++)
+            {
+                B:
+                a = messi.Next(0, 9);
+                if (i == 0 && a == 0) goto B;
+                Console.Write(a);
+            }
+            Console.Write(" SOCIAL CREDIT!");
+            Console.ReadKey();
+        }
+    }
+}
